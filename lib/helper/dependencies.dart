5309f33a05ce985e1691320f80f0c2ac29726b5e
199soumallya <1999soumallya@gmail.com>
2022-11-30 18:48:42 +0530

    Add Model

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,16 @@
+import 'package:food_delivery_app/Controller/popular_product_controller.dart';
+import 'package:food_delivery_app/data/api/api_client.dart';
+import 'package:food_delivery_app/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
 Future<void> init() async{
+  // Api Client
+  Get.lazyPut(() => ApiClient(appBaseUrl: "https://www.dbestech.com"));
+
+  // Repository
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  // Controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:food_delivery_app/pages/food/recommened_food_detail.dart';
 import 'package:food_delivery_app/pages/home/main_food_page.dart';
 import 'package:food_delivery_app/pages/food/popular_food_detail.dart';
 import 'package:get/get.dart';
+import 'package:food_delivery_app/helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
